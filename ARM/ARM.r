ebc8e9c8226fdee88a651213f1bc82746bb1494a
ErcongLuo <luoercong@gmail.com>
2021-10-26 22:56:40 -0400

    ARM first commit

---
+++ ARM/ARM.r
@@ -0,0 +1,20 @@
+################################################ 
+######### Author: Ercong Luo  ##################
+######### Last Updated: 10/26/21 ###############
+#########    ARM    ############################
+################################################
+
+# Read in Data
+df = read.csv(file = "/Users/ercongluo/Documents/GU/501/501portfolio/datasets/NewsData/NewsData_cleanedDF.csv")
+
+# Setting up libraries 
+library(arules, arulesViz)
+library(igraph)
+library(networkD3)
+library(visNetwork)
+library(tidyverse)
+library(dployr)
+
+
+
+
